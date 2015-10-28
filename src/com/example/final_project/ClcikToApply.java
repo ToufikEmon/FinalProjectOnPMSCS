4c3c13075f0f662bc9c98592b1a28616d23fb8a1
Toufik Ahmed Emon <ahmedemoncse20@gmail.com>
2015-10-28 23:32:37 +0600

    7th commit

--- src/com/example/final_project/ClcikToApply.java
+++ src/com/example/final_project/ClcikToApply.java
@@ -35,20 +35,22 @@ public class ClcikToApply extends Activity {
 
 	private ProgressDialog pDialog;
 	JSONParser jsonParser = new JSONParser();
-	Spinner spSex,spBloodGroup;
+	Spinner spSex, spBloodGroup;
 
 	EditText edtApplicantName, edtFatherName, edtMotherName, edtNationality,
-			edtPermanentAddress,edtPresentAddress, edtContactNumber,edtEmail,edtDateOfBirth;
+			edtPermanentAddress, edtPresentAddress, edtContactNumber, edtEmail,
+			edtDateOfBirth, edtSSC, edtHSC, edtHons, edtOthers, edtGroupSSC,
+			edtGroupHSC, edtGroupHons, edtGroupOther, edtBoardSSC, edtBoardHSC,
+			edtBoardHons, edtBoardOthers, edtSSCCGPA, edtHSCCGPA, edtHONSCGPA,
+			edtOthersCGPA,edtSSCPassingYear, edtHSCPassingYear,edtHonsPassingYear, edtOthersPassingyear,edtPresentStatusAndOrganization, edtExperience;
 	// ,
-	// , edtSSC, edtHSC, edtHons, edtOthers, edtGroupSSC,
-	// edtGroupHSC, edtGroupHons, edtGroupOther, edtBoardSSC, edtBoardHSC,
-	// edtBoardHons, edtBoardOthers, edtSSCPassingYear, edtHSCPassingYear,
-	// edtHonsPassingYear, edtOthersPassingyear,
+	// ,
+	
 	// edtPresentStatusAndOrganization, edtExperience;
 
 	Button btnSubmit;
 
-	private static String url_insert_into_database = "http://10.62.32.213/final_project/insert_data.php";
+	private static String url_insert_into_database = "http://10.63.145.199/final_project/insert_data.php";
 
 	// JSON Node names
 	private static final String TAG_SUCCESS = "success";
@@ -75,29 +77,33 @@ public class ClcikToApply extends Activity {
 		edtPermanentAddress = (EditText) findViewById(R.id.edtPermanentAddress);
 		edtPresentAddress = (EditText) findViewById(R.id.edtPresentAddress);
 		edtContactNumber = (EditText) findViewById(R.id.edtContactNumber);
-		 edtEmail = (EditText) findViewById(R.id.edtEmail);
-		 edtDateOfBirth = (EditText) findViewById(R.id.edtDateOfBirth);
-		 //spSex = (Spinner) findViewById(R.id.spSex);
-		// spBloodGroup = (Spinner) findViewById(R.id.spBloodGroup);
-		// edtSSC = (EditText) findViewById(R.id.edtSSC);
-		// edtHSC = (EditText) findViewById(R.id.edtHSC);
-		// edtHons = (EditText) findViewById(R.id.edtHons);
-		// edtOthers = (EditText) findViewById(R.id.edtOthers);
-		// edtGroupSSC = (EditText) findViewById(R.id.edtSSCGroup);
-		// edtGroupHSC = (EditText) findViewById(R.id.edtHSCGroup);
-		// edtGroupHons = (EditText) findViewById(R.id.edtHonsGroup);
-		// edtGroupOther = (EditText) findViewById(R.id.edtOtherGroup);
-		// edtBoardSSC = (EditText) findViewById(R.id.edtSSCBoard);
-		// edtBoardHSC = (EditText) findViewById(R.id.edtHSCBoard);
-		// edtBoardHons = (EditText) findViewById(R.id.edtHonsBoard);
-		// edtBoardOthers = (EditText) findViewById(R.id.edtOtherBoard);
-		// edtSSCPassingYear = (EditText) findViewById(R.id.edtSSCYear);
-		// edtHSCPassingYear = (EditText) findViewById(R.id.edtHSCYear);
-		// edtHonsPassingYear = (EditText) findViewById(R.id.edtHONSYear);
-		// edtOthersPassingyear = (EditText) findViewById(R.id.edtOtherYear);
-		// edtPresentStatusAndOrganization = (EditText)
-		// findViewById(R.id.edtPresentStatusAndOrganization);
-		// edtExperience = (EditText) findViewById(R.id.edtPresentExperience);
+		edtEmail = (EditText) findViewById(R.id.edtEmail);
+		edtDateOfBirth = (EditText) findViewById(R.id.edtDateOfBirth);
+		spSex = (Spinner) findViewById(R.id.spSex);
+		spBloodGroup = (Spinner) findViewById(R.id.spBloodGroup);
+		edtSSC = (EditText) findViewById(R.id.edtSSC);
+		edtHSC = (EditText) findViewById(R.id.edtHSC);
+		edtHons = (EditText) findViewById(R.id.edtHons);
+		edtOthers = (EditText) findViewById(R.id.edtOthers);
+		edtGroupSSC = (EditText) findViewById(R.id.edtSSCGroup);
+		edtGroupHSC = (EditText) findViewById(R.id.edtHSCGroup);
+		edtGroupHons = (EditText) findViewById(R.id.edtHonsGroup);
+		edtGroupOther = (EditText) findViewById(R.id.edtOtherGroup);
+		edtBoardSSC = (EditText) findViewById(R.id.edtSSCBoard);
+		edtBoardHSC = (EditText) findViewById(R.id.edtHSCBoard);
+		edtBoardHons = (EditText) findViewById(R.id.edtHonsBoard);
+		edtBoardOthers = (EditText) findViewById(R.id.edtOtherBoard);
+		edtSSCCGPA = (EditText) findViewById(R.id.edtSSCCGPA);
+		edtHSCCGPA = (EditText) findViewById(R.id.edtHSCCGPA);
+		edtHONSCGPA = (EditText) findViewById(R.id.edtHONSCGPA);
+		edtOthersCGPA = (EditText) findViewById(R.id.edtOtherSCGPA);
+	    edtSSCPassingYear = (EditText) findViewById(R.id.edtSSCYear);
+		 edtHSCPassingYear = (EditText) findViewById(R.id.edtHSCYear);
+		 edtHonsPassingYear = (EditText) findViewById(R.id.edtHONSYear);
+		 edtOthersPassingyear = (EditText) findViewById(R.id.edtOtherYear);
+		 edtPresentStatusAndOrganization = (EditText)
+		 findViewById(R.id.edtPresentStatusAndOrganization);
+		 edtExperience = (EditText) findViewById(R.id.edtPresentExperience);
 
 		btnSubmit = (Button) findViewById(R.id.btnSubmit);
 		btnSubmit.setOnClickListener(new OnClickListener() {
@@ -161,31 +167,35 @@ public class ClcikToApply extends Activity {
 
 			String nationality = edtNationality.getText().toString();
 			String permanent_address = edtPermanentAddress.getText().toString();
-			 String present_address = edtPresentAddress.getText().toString();
-			 String contact_number = edtContactNumber.getText().toString();
-			 String email = edtEmail.getText().toString();
-			 String date_of_birth = edtDateOfBirth.getText().toString();
-			// String sex = spSex.getItemIdAtPosition(0).toString();
-			// String blood_group = spBloodGroup.getContext().toString();
-			// String degree1 = edtSSC.getText().toString();
-			// String degree2 = edtHSC.getText().toString();
-			// String degree3 = edtHons.getText().toString();
-			// String degree4 = edtOthers.getText().toString();
-			// String group1 = edtGroupSSC.getText().toString();
-			// String group2 = edtGroupHSC.getText().toString();
-			// String group3 = edtGroupHons.getText().toString();
-			// String group4 = edtGroupOther.getText().toString();
-			// String board1 = edtBoardSSC.getText().toString();
-			// String board2 = edtBoardHSC.getText().toString();
-			// String board3 = edtBoardHons.getText().toString();
-			// String board4 = edtBoardOthers.getText().toString();
-			// String passing_year1 = edtSSCPassingYear.getText().toString();
-			// String passing_year2 = edtHSCPassingYear.getText().toString();
-			// String passing_year3 = edtHonsPassingYear.getText().toString();
-			// String passing_year4 = edtOthersPassingyear.getText().toString();
-			// String status = edtPresentStatusAndOrganization.getText()
-			// .toString();
-			// String organization = edtExperience.getText().toString();
+			String present_address = edtPresentAddress.getText().toString();
+			String contact_number = edtContactNumber.getText().toString();
+			String email = edtEmail.getText().toString();
+			String date_of_birth = edtDateOfBirth.getText().toString();
+			String sex = spSex.getContext().toString();
+			String blood_group = spBloodGroup.getContext().toString();
+			String degree1 = edtSSC.getText().toString();
+			String degree2 = edtHSC.getText().toString();
+			String degree3 = edtHons.getText().toString();
+			String degree4 = edtOthers.getText().toString();
+			String group1 = edtGroupSSC.getText().toString();
+			String group2 = edtGroupHSC.getText().toString();
+			String group3 = edtGroupHons.getText().toString();
+			String group4 = edtGroupOther.getText().toString();
+			String board1 = edtBoardSSC.getText().toString();
+			String board2 = edtBoardHSC.getText().toString();
+			String board3 = edtBoardHons.getText().toString();
+			String board4 = edtBoardOthers.getText().toString();
+			String ssc_cgpa = edtSSCCGPA.getText().toString();
+			String hsc_cgpa = edtHSCCGPA.getText().toString();
+			String hons_cgpa = edtHONSCGPA.getText().toString();
+			String others_cgpa = edtOthersCGPA.getText().toString();
+			 String passing_year1 = edtSSCPassingYear.getText().toString();
+			 String passing_year2 = edtHSCPassingYear.getText().toString();
+			 String passing_year3 = edtHonsPassingYear.getText().toString();
+			 String passing_year4 = edtOthersPassingyear.getText().toString();
+			 String status = edtPresentStatusAndOrganization.getText()
+			 .toString();
+			 String organization = edtExperience.getText().toString();
 
 			List<NameValuePair> params = new ArrayList<NameValuePair>();
 			params.add(new BasicNameValuePair("applicant_name", applicant_name));
@@ -194,36 +204,39 @@ public class ClcikToApply extends Activity {
 			params.add(new BasicNameValuePair("nationality", nationality));
 			params.add(new BasicNameValuePair("permanent_address",
 					permanent_address));
-			 params.add(new BasicNameValuePair("present_address",
-			 present_address));
-			 params.add(new BasicNameValuePair("contact_number",
-			 contact_number));
-			 params.add(new BasicNameValuePair("email", email));
-			 params.add(new BasicNameValuePair("date_of_birth", date_of_birth));
-			// params.add(new BasicNameValuePair("sex",sex));
-			// params.add(new BasicNameValuePair("blood_group",blood_group));
-			// params.add(new BasicNameValuePair("degree1", degree1));
-			// params.add(new BasicNameValuePair("degree2", degree2));
-			// params.add(new BasicNameValuePair("degree3", degree3));
-			// params.add(new BasicNameValuePair("degree4", degree4));
-			// params.add(new BasicNameValuePair("group1", group1));
-			// params.add(new BasicNameValuePair("group2", group2));
-			// params.add(new BasicNameValuePair("group3", group3));
-			// params.add(new BasicNameValuePair("group4", group4));
-			// params.add(new BasicNameValuePair("board1", board1));
-			// params.add(new BasicNameValuePair("board2", board2));
-			// params.add(new BasicNameValuePair("board3", board3));
-			// params.add(new BasicNameValuePair("board4", board4));
-			// params.add(new BasicNameValuePair("passing_year1",
-			// passing_year1));
-			// params.add(new BasicNameValuePair("passing_year2",
-			// passing_year2));
-			// params.add(new BasicNameValuePair("passing_year3",
-			// passing_year3));
-			// params.add(new BasicNameValuePair("passing_year4",
-			// passing_year4));
-			// params.add(new BasicNameValuePair("organization", organization));
-			// params.add(new BasicNameValuePair("status", status));
+			params.add(new BasicNameValuePair("present_address",
+					present_address));
+			params.add(new BasicNameValuePair("contact_number", contact_number));
+			params.add(new BasicNameValuePair("email", email));
+			params.add(new BasicNameValuePair("date_of_birth", date_of_birth));
+			params.add(new BasicNameValuePair("sex", sex));
+			params.add(new BasicNameValuePair("blood_group", blood_group));
+			params.add(new BasicNameValuePair("degree1", degree1));
+			params.add(new BasicNameValuePair("degree2", degree2));
+			params.add(new BasicNameValuePair("degree3", degree3));
+			params.add(new BasicNameValuePair("degree4", degree4));
+			params.add(new BasicNameValuePair("group1", group1));
+			params.add(new BasicNameValuePair("group2", group2));
+			params.add(new BasicNameValuePair("group3", group3));
+			params.add(new BasicNameValuePair("group4", group4));
+			params.add(new BasicNameValuePair("board1", board1));
+			params.add(new BasicNameValuePair("board2", board2));
+			params.add(new BasicNameValuePair("board3", board3));
+			params.add(new BasicNameValuePair("board4", board4));
+			params.add(new BasicNameValuePair("ssc_cgpa", ssc_cgpa));
+			params.add(new BasicNameValuePair("hsc_cgpa", hsc_cgpa));
+			params.add(new BasicNameValuePair("hons_cgpa", hons_cgpa));
+			params.add(new BasicNameValuePair("others_cgpa", others_cgpa));
+			 params.add(new BasicNameValuePair("passing_year1",
+			 passing_year1));
+			 params.add(new BasicNameValuePair("passing_year2",
+			 passing_year2));
+			 params.add(new BasicNameValuePair("passing_year3",
+			 passing_year3));
+			 params.add(new BasicNameValuePair("passing_year4",
+			 passing_year4));
+			 params.add(new BasicNameValuePair("organization", organization));
+			 params.add(new BasicNameValuePair("status", status));
 
 			JSONObject json = jsonParser.makeHttpRequest(
 					url_insert_into_database, "POST", params);
